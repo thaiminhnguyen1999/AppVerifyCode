93c6e030b0af6ac29ae157c0b1ff6d0ab432e6f4
Nguyễn Thái Minh <nguyenthaiminh0973200965@gmail.com>
2024-09-07 16:16:44 +0700

    Commit files

--- api.js
+++ api.js
@@ -105,7 +105,7 @@ app.post('/api/otpVerification', async (req, res) => {
 
     if (row) {
         const randomCode = Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000;
-        const message = `Your verification code is ||*${randomCode}*||\\. Please keep it secret and don\\'t share it with anyone\\.\\\nCode sent by *${row[1]} \\(${row[2]}\\)\\*\\.`;
+        const message = `Your verification code is \\|\\|\\*${randomCode}\\*\\|\\|\\. Please keep it secret and don\\'t share it with anyone\\.\\\nCode sent by \\*${row[1]} \\(${row[2]}\\)\\*\\.`;
 
 
         try {
