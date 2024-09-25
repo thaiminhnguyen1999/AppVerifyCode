f2eea32312ed09392ebd83db6989b458b1e48ea9
Nguyễn Thái Minh <nguyenthaiminh0973200965@gmail.com>
2024-09-25 19:10:24 +0700

    Commit files

--- api.js
+++ api.js
@@ -1,18 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios');
 const { google } = require('googleapis');
-const nodemailer = require('nodemailer');
-const TelegramBot = require('node-telegram-bot-api');
 const { OAuth2Client } = require('google-auth-library');
-const path = require('path');
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+const nodemailer = require('nodemailer');
+
+const telegramApiUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`;
 
 const app = express();
 app.use(express.json());
 
 const SHEET_ID = process.env.SHEET_ID;
-const RANGE = "Lists!A2:D";
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const RANGE = "avc-app!A2:E";
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 const auth = new google.auth.GoogleAuth({
     keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
     scopes: SCOPES
@@ -23,27 +24,84 @@ const oAuth2Client = new OAuth2Client(
     process.env.OAUTH_CLIENT_SECRET,
     'https://developers.google.com/oauthplayground'
 );
+
 oAuth2Client.setCredentials({
     refresh_token: process.env.OAUTH_REFRESH_TOKEN
 });
 
-const telegramApiUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`;
 const API_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+if (!API_TOKEN) {
+    console.error("Error: TELEGRAM_BOT_TOKEN is not defined in the .env file.");
+    process.exit(1);
+}
+
 const bot = new TelegramBot(API_TOKEN, { polling: true });
 
-async function createTransporter() {
-    const accessToken = await oAuth2Client.getAccessToken();
-    return nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'OAuth2',
-            user: process.env.EMAIL_USER,
-            clientId: process.env.OAUTH_CLIENT_ID,
-            clientSecret: process.env.OAUTH_CLIENT_SECRET,
-            refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-            accessToken: accessToken.token
+const getGoogleSheetsClient = async () => {
+    const client = await auth.getClient();
+    return google.sheets({ version: 'v4', auth: client });
+};
+
+async function getSheetData() {
+    const sheets = await getGoogleSheetsClient();
+    const res = await sheets.spreadsheets.values.get({
+        spreadsheetId: SHEET_ID,
+        range: RANGE,
+    });
+    return res.data.values;
+}
+
+async function updateSheet(rowIndex, data) {
+    const sheets = await getGoogleSheetsClient();
+    await sheets.spreadsheets.values.update({
+        spreadsheetId: SHEET_ID,
+        range: `avc-app!A${rowIndex}:E`,
+        valueInputOption: 'RAW',
+        resource: { values: [data] }
+    });
+}
+
+async function deleteFromSheet(rowIndex) {
+    const sheets = await getGoogleSheetsClient();
+    await sheets.spreadsheets.batchUpdate({
+        spreadsheetId: SHEET_ID,
+        resource: {
+            requests: [{
+                deleteDimension: {
+                    range: {
+                        sheetId: 0,
+                        dimension: 'ROWS',
+                        startIndex: rowIndex - 1,
+                        endIndex: rowIndex
+                    }
+                }
+            }]
         }
     });
+}
+
+async function appendToSheet(data) {
+    const sheets = await getGoogleSheetsClient();
+    await sheets.spreadsheets.values.append({
+        spreadsheetId: SHEET_ID,
+        range: 'avc-app!A:E',
+        valueInputOption: 'RAW',
+        resource: { values: [data] }
+    });
+}
+
+function authkeyGenerator(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const nums = '0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        if (Math.random() < 0.75) {
+            result += chars.charAt(Math.floor(Math.random() * chars.length));
+        } else {
+            result += nums.charAt(Math.floor(Math.random() * nums.length));
+        }
+    }
+    return result;
 }
 
 function escapeMarkdownV2(text) {
@@ -68,135 +126,41 @@ function escapeMarkdownV2(text) {
         .replace(/\}/g, '\\}');
 }
 
-bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    const message = `Your ChatID is *${chatId}*\\. Please enter this ChatID into the app to receive OTP verification code\\.`;
-    bot.sendMessage(chatId, message, { parse_mode: 'MarkdownV2' });
-});
-
-bot.onText(/\/create/, (msg) => {
-    const chatId = msg.chat.id;
-    const responseText = "Welcome to *AppVerify Code*\\, a *free* OTP verification code sending service for *individuals and businesses*\\.\n" +
-        "Register to use *AppVerify Code*\\'s API with a little understanding of API usage\\.";
-
-    const options = {
-        reply_markup: {
-            inline_keyboard: [
-                [{ text: "✔ Register", url: "https://appverifycode.glide.page" }],
-                [{ text: "📃 Document", url: "https://hitech-corporation.gitbook.io/appverifycode-api/" }]
-            ]
-        },
-        parse_mode: 'MarkdownV2'
-    };
-
-    bot.sendMessage(chatId, responseText, options);
-});
-
-async function getSheetData() {
-    const client = await auth.getClient();
-    const sheets = google.sheets({ version: 'v4', auth: client });
-    const res = await sheets.spreadsheets.values.get({
-        spreadsheetId: SHEET_ID,
-        range: RANGE,
+async function createTransporter() {
+    const accessToken = await oAuth2Client.getAccessToken();
+    return nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: 'OAuth2',
+            user: process.env.EMAIL_USER,
+            clientId: process.env.OAUTH_CLIENT_ID,
+            clientSecret: process.env.OAUTH_CLIENT_SECRET,
+            refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+            accessToken: accessToken.token
+        }
     });
-    return res.data.values;
 }
 
-app.get('/api/telegram/otpVerification', (_req, res) => {
-    return res.status(405).json({
-        "error": "GET method is not supported for /api/telegram/otpVerification. Do you mean /api/getData?"
-    });
-});
+function isAllowedDomain(domain) {
+    const allowedDomains = [
+        'hotmail.com',
+        'outlook.com.vn',
+        'outlook.com',
+        'gmail.com',
+        'yahoo.com',
+        'microsoft.com',
+        'proton.me',
+        'protonmail.com',
+        'icloud.com',
+        'apple.com'
+    ];
+    return allowedDomains.includes(domain.toLowerCase());
+}
 
-app.get('/api/email/otpVerification', (_req, res) => {
-    return res.status(405).json({
-        "error": "GET method is not supported for /api/email/otpVerification. Do you mean /api/getData?"
-    });
-});
-
-app.get('/api/getData', async (req, res) => {
-    const { avc_authkey } = req.query;
-    if (!avc_authkey) {
-        return res.status(400).json({ error: 'avc_authkey is required' });
-    }
-
-    const rows = await getSheetData();
-    const row = rows.find(r => r[3] === avc_authkey);
-
-    if (row) {
-        const response = {
-            "restful-method": "GET",
-            "response": "200",
-            "avc_authkey": avc_authkey,
-            "status": "found",
-            "authkey_info": {
-                "tg_username": row[0],
-                "tg_appname": row[1],
-                "tg_appbot": row[2]
-            }
-        };
-        return res.json(response);
-    } else {
-        return res.status(404).json({ 
-            "restful-method": "GET",
-            "response": "404",
-
-            "error": "avc_authkey not found" 
-        });
-    }
-});
-
-app.post('/api/telegram/otpVerification', async (req, res) => {
-    const { avc_authkey, chat_id } = req.query;
-    if (!avc_authkey || !chat_id) {
-        return res.status(400).json({ error: 'avc_authkey and chat_id are required' });
-    }
-
-    const rows = await getSheetData();
-    const row = rows.find(r => r[3] === avc_authkey);
-
-    if (row) {
-        const randomCode = Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000;
-
-        const message = `Your verification code is ||*${escapeMarkdownV2(randomCode.toString())}*||\\. Please keep it secret and don't share it with anyone\\.\nCode sent by *${escapeMarkdownV2(row[1])}* \\(${escapeMarkdownV2(row[2])}\\)\\.`;
-
-        try {
-            const telegramRes = await axios.post(telegramApiUrl, {
-                chat_id: chat_id,
-                text: message,
-                parse_mode: 'MarkdownV2'
-            });
-
-            if (telegramRes.status === 200) {
-                return res.json({
-                    "restful-method": "POST",
-                    "response": "200",
-                    "status": "found",
-                    "authkey_info": {
-                        "tg_username": row[0],
-                        "tg_appname": row[1],
-                        "tg_appbot": row[2]
-                    },
-                    "sendTo": {
-                        "chatID": chat_id,
-                        "username": row[0]
-                    },
-                    "verificationCode": randomCode
-                });
-            } else {
-                return res.status(500).json({ error: 'Failed to send Telegram message' });
-            }
-        } catch (error) {
-            return res.status(500).json({ error: 'Telegram API error', details: error.message });
-        }
-    } else {
-        return res.status(404).json({
-            "restful-method": "POST",
-            "response": "404",
-
-            "error": "avc_authkey not found"
-        });
-    }
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    const message = `Your ChatID is <b>${chatId.toString()}</b>. Please enter this ChatID into the app to receive OTP verification code.`;
+    bot.sendMessage(chatId, message, { parse_mode: 'HTML' });
 });
 
 app.post('/api/email/otpVerification', async (req, res) => {
@@ -205,52 +169,57 @@ app.post('/api/email/otpVerification', async (req, res) => {
         return res.status(400).json({ error: 'avc_authkey and email are required' });
     }
 
+    const domain = email.split('@')[1];
     const rows = await getSheetData();
     const row = rows.find(r => r[3] === avc_authkey);
 
     if (row) {
-        const randomCode = Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000;
+        if (isAllowedDomain(domain) || row[4] === 'actived') {
+            const randomCode = Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000;
+            const sanitizedTelegramBot = row[2].replace(/@/g, '');
 
-        const sanitizedTelegramBot = row[2].replace(/@/g, '');
+            const transporter = await createTransporter();
+            const mailOptions = {
+                from: process.env.EMAIL_USER,
+                to: email,
+                subject: 'AppVerify Code - OTP Verification',
+                html:
+                    `<div style="text-align: center; font-weight: bold;"><h1>AppVerify Code</h1></div>
+                    <p>Your verification code is <strong>${randomCode}</strong>. Please keep it secret and don't share it with anyone.</p>
+                    <p>Code sent by <strong>${row[1]} (<a href="https://t.me/${sanitizedTelegramBot}">${row[2]}</a>)</strong>.</p>
+                    <img src="cid:mailfooter" alt="Mail Footer" style="width: 100% !important; min-width: 100% !important;" />`,
+                attachments: [{
+                    filename: 'mail-footer.jpg',
+                    path: './mail-footer.jpg',
+                    cid: 'mailfooter'
+                }]
+            };
 
-        const transporter = await createTransporter();
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: email,
-            subject: 'AppVerify Code - OTP Verification',
-            html: `
-                <div style="text-align: center; font-weight: bold;"><h1>AppVerify Code</h1></div>
-                <p>Your verification code is <strong>${randomCode}</strong>. Please keep it secret and don't share it with anyone.</p>
-                <p>Code sent by <strong>${row[1]} (<a href="https://t.me/${sanitizedTelegramBot}">${row[2]}</a>)</strong>.</p>
-                <img src="cid:mailfooter" alt="Mail Footer" style="width: 100% !important; min-width: 100% !important;" />
-            `,
-            attachments: [{
-                filename: 'mail-footer.jpg',
-                path: './mail-footer.jpg',
-                cid: 'mailfooter'
-            }]
-        };
-
-        transporter.sendMail(mailOptions, (error) => {
-            if (error) {
-                return res.status(500).json({ error: 'Failed to send email', details: error.message });
-            } else {
-                return res.json({
-                    "restful-method": "POST",
-                    "response": "200",
-                    "status": "found",
-                    "authkey_info": {
-                        "tg_username": row[0],
-                        "tg_appname": row[1],
-                        "tg_appbot": row[2]
-                    },
-                    "sendTo": {
-                        "email": email
-                    },
-                    "verificationCode": randomCode
-                });
-            }
-        });
+            transporter.sendMail(mailOptions, (error) => {
+                if (error) {
+                    return res.status(500).json({ error: 'Failed to send email', details: error.message });
+                } else {
+                    return res.json({
+                        "restful-method": "POST",
+                        "response": "200",
+                        "status": "found",
+                        "authkey_info": {
+                            "tg_username": row[0],
+                            "tg_appname": row[1],
+                            "tg_appbot": row[2]
+                        },
+                        "sendTo": {
+                            "email": email
+                        },
+                        "verificationCode": randomCode
+                    });
+                }
+            });
+        } else {
+            return res.status(403).json({
+                error: "User's email address contains business domain name. To send OTP Verification Code, account must have the AVC+ package activated."
+            });
+        }
     } else {
         return res.status(404).json({
             "restful-method": "POST",
@@ -259,7 +228,6 @@ app.post('/api/email/otpVerification', async (req, res) => {
         });
     }
 });
-
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT || 3000}`);
